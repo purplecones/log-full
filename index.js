b4e311cf0ecc6d93638b4ff520aa7338f14c32ca
Mirza Joldic <mirza.joldic@gmail.com>
2017-03-23 14:40:58 -0400

    extending console object instead

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const util = require('util');
 
-module.exports = function (data, options = null) {
+console.full = function (data, options = null) {
   if (!options) {
     options = {
       depth: 4,
       colors: true,
     };
   };
-
   console.log(util.inspect(data, options))
 }
